bc339e1d5be8d8115eea3ec651d3036e22f11e19
Intro <72384746+Intro-iu@users.noreply.github.com>
2024-05-07 08:58:15 +0800

    Update main.cpp

--- session/main.cpp
+++ session/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     // putenv((char *)"SESSION_MANAGER=");
 
     // force xcb QPA plugin as session manager server is very X11 specific.
-    qputenv("QT_QPA_PLATFORM", QByteArrayLiteral("wayland"));
+    qputenv("QT_QPA_PLATFORM", QByteArrayLiteral("xcb"));
 
     QQuickWindow::setDefaultAlphaBuffer(true);
     QCoreApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
